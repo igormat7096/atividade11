04ecf775aa23f99dff0acbbe9f5818030974dd47
igormat7096 <igornovais.tj96@gmail.com>
2024-10-10 21:00:45 -0300

    atividade_11

---
+++ app.py
@@ -0,0 +1,17 @@
+#Crie um programa que receba do usuário os valores "a" e "b" e envie para uma função que calcule a função do 1º grau. Ao final, o programa mostre o valor de "x".
+
+# Programa principal :
+
+print("calcular a função em 1 grau")
+
+def calcular_a_funcao_de_1_grau(a1, b2):
+    return -b2 / a1
+
+a1 = float(input("Digite o valor de 'A': "))
+b2 = float(input("Digite o valor de 'B': "))
+
+if a1 != 0:
+    x = calcular_a_funcao_de_1_grau(a1, b2)
+    print(f"O valor de 'X' é: {x}")
+else:
+    print("O valor de 'A' não pode ser zero.")
